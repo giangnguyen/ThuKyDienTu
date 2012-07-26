9cb25d070a66af6cf8634f77206f496c66d9f03d
Giang Nguyen <giangnguyen@esofthead.com>
2012-07-26 08:50:51 +0700

    - new commit

--- Main/src/myapp/thukydientu/view/HomeActivity.java
+++ Main/src/myapp/thukydientu/view/HomeActivity.java
@@ -1,10 +1,16 @@
 package myapp.thukydientu.view;
 
 import myapp.thukydientu.R;
+import myapp.thukydientu.util.TimeUtils;
+import myapp.thukydientu.util.WebservicesUtils;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -14,46 +20,115 @@ import android.widget.Toast;
 public class HomeActivity extends Activity {
 
 	public static final int DIALOG_INFORM_INPUT = 1;
+	public static final int DIALOG_SHARE = 2;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home);
-		
+
+		View share = findViewById(R.id.share);
+		share.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				showDialog(DIALOG_SHARE);
+			}
+		});
+
 		View inform = findViewById(R.id.inform);
 		inform.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				showDialog(DIALOG_INFORM_INPUT);
 			}
 		});
+
 	}
 
 	@Override
 	protected Dialog onCreateDialog(int id) {
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		
-		View informView = LayoutInflater.from(this).inflate(R.layout.inform, null);
-		
-		final EditText title = (EditText) informView.findViewById(R.id.title);
-		final EditText content = (EditText) informView.findViewById(R.id.content);
-		
-		Button submit = (Button) informView.findViewById(R.id.submit);
-		submit.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				final String titleString = title.getText().toString();
-				final String contentString = content.getText().toString();
-				Toast.makeText(HomeActivity.this, "title: " + titleString + " content: " + contentString, Toast.LENGTH_LONG).show();
-			}
-		});
-		
-		builder.setView(informView);
-		
-		return builder.create();
+
+		switch (id) {
+
+		case DIALOG_INFORM_INPUT:
+
+			View informView = LayoutInflater.from(this).inflate(
+					R.layout.inform, null);
+
+			final EditText title = (EditText) informView
+					.findViewById(R.id.title);
+			final EditText content = (EditText) informView
+					.findViewById(R.id.content);
+
+			Button submit = (Button) informView.findViewById(R.id.submit);
+			submit.setOnClickListener(new View.OnClickListener() {
+
+				@Override
+				public void onClick(View v) {
+					final String titleString = title.getText().toString();
+					final String contentString = content.getText().toString();
+					Toast.makeText(
+							HomeActivity.this,
+							"title: " + titleString + " content: "
+									+ contentString, Toast.LENGTH_LONG).show();
+				}
+			});
+
+			builder.setView(informView);
+
+			return builder.create();
+
+		case DIALOG_SHARE:
+
+			View shareView = LayoutInflater.from(this).inflate(
+					R.layout.share_dialog, null);
+
+			builder.setTitle(R.string.share);
+			builder.setView(shareView);
+
+			return builder.create();
+		default:
+			return null;
+		}
 	}
-	
-	
+
+	public void shareLocation() {
+		final LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+		LocationListener locationListener = new LocationListener() {
+
+			@Override
+			public void onStatusChanged(String provider, int status,
+					Bundle extras) {
+
+			}
+
+			@Override
+			public void onProviderEnabled(String provider) {
+
+			}
+
+			@Override
+			public void onProviderDisabled(String provider) {
+
+			}
+
+			@Override
+			public void onLocationChanged(Location location) {
+				Log.d("Check Location: ", "latitude: " + location.getLatitude());
+				Log.d("Check Location: ",
+						"longtitude: " + location.getLongitude());
+				locationManager.removeUpdates(this);
+			}
+		};
+		locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+		Location lc = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+		final double latitude = lc.getLatitude();
+		final double longitude = lc.getLongitude();
+		WebservicesUtils.addLocation(MainActivity.sUserId, latitude, longitude, false, TimeUtils.convert2String14(System.currentTimeMillis()));
+
+	}
 
 }
--- Main/src/myapp/thukydientu/view/TodoListActivity.java
+++ Main/src/myapp/thukydientu/view/TodoListActivity.java
@@ -4,17 +4,10 @@ import myapp.thukydientu.R;
 import myapp.thukydientu.adapter.TodoAdapter;
 import myapp.thukydientu.model.IConstants;
 import myapp.thukydientu.service.SyncService;
-import myapp.thukydientu.util.TimeUtils;
 import myapp.thukydientu.util.TodoUtils;
-import myapp.thukydientu.util.WebservicesUtils;
 import android.app.ListActivity;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -22,18 +15,13 @@ import android.view.MenuItem;
 public class TodoListActivity extends ListActivity {
 
 	private TodoAdapter mAdapter;
-	private int userId;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.todo_list);
 
-		SharedPreferences prefs = getSharedPreferences(IConstants.PREF_NAME,
-				MODE_PRIVATE);
-		userId = prefs.getInt(IConstants.User.ID, 0);
-
-		mAdapter = new TodoAdapter(this, userId);
+		mAdapter = new TodoAdapter(this, MainActivity.sUserId);
 		setListAdapter(mAdapter);
 
 	}
@@ -60,52 +48,10 @@ public class TodoListActivity extends ListActivity {
 			break;
 		}
 		case 2:
-			new SyncService(this, userId, IConstants.DataType.TODO);
-			break;
-		case 3:
-			shareLocation();
+			new SyncService(this, MainActivity.sUserId, IConstants.DataType.TODO);
 			break;
 		}
 		return true;
-
-	}
-
-	public void shareLocation() {
-		final LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-		LocationListener locationListener = new LocationListener() {
-
-			@Override
-			public void onStatusChanged(String provider, int status,
-					Bundle extras) {
-
-			}
-
-			@Override
-			public void onProviderEnabled(String provider) {
-
-			}
-
-			@Override
-			public void onProviderDisabled(String provider) {
-
-			}
-
-			@Override
-			public void onLocationChanged(Location location) {
-				Log.d("Check Location: ", "latitude: " + location.getLatitude());
-				Log.d("Check Location: ",
-						"longtitude: " + location.getLongitude());
-				locationManager.removeUpdates(this);
-			}
-		};
-		locationManager.requestLocationUpdates(
-				LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
-		Location lc = locationManager
-				.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-		double latitude = lc.getLatitude();
-		double longitude = lc.getLongitude();
-		WebservicesUtils.addLocation(userId, latitude, longitude, false,
-				TimeUtils.convert2String14(System.currentTimeMillis()));
 
 	}
 
@@ -117,8 +63,6 @@ public class TodoListActivity extends ListActivity {
 		deleteItem.setIcon(R.drawable.ic_menu_clear_list);
 		MenuItem sync = menu.add(0, 2, 2, "Đồng Bộ");
 		sync.setIcon(R.drawable.sync);
-		MenuItem shareLocation = menu.add(0, 3, 3, "Chia Sẽ Vị Trí");
-		shareLocation.setIcon(android.R.drawable.ic_menu_mylocation);
 
 	}
 
